84d8dcf82d706f4c6666104005153f0a2f6fd4bd
Natasha Ivanova <terehinann@gmail.com>
2023-02-14 10:05:17 +0300

    fix test bugs

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(bodyParser.json()); // для собирания JSON-формата
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
-    password: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(URL_REGEX),
